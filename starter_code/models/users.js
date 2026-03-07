c3073b3b811b1fff6fdbc60b67cf09180e59c595
Heba Sajid <hsajid35467@gmail.com>
2026-03-06 20:27:29 -0700

    adding logic for finding a user via username for login purposes.

--- starter_code/models/users.js
+++ starter_code/models/users.js
@@ -1,2 +1,18 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('database.db');
+
+const UsersModel = {
+    // finding a user by their username (for the Login task)
+    findByUsername: (username) => {
+        return new Promise((resolve, reject) => {
+            db.get("SELECT * FROM Users WHERE username = ?", [username], (err, row) => {
+                if (err) reject(err);
+                resolve(row);
+            });
+        });
+    },
+
+    
+};
+
+module.exports = UsersModel;
