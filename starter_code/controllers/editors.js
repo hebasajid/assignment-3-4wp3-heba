38bb4ef8c6821e5ff243a5731f888ffb5f347633
Heba Sajid <hsajid35467@gmail.com>
2026-03-14 23:18:45 -0600

    on editoes template created the table, fixed rendering logic in editors.js to display the articles and users

--- starter_code/controllers/editors.js
+++ starter_code/controllers/editors.js
@@ -1,28 +1,32 @@
 const express = require('express');
-var router = express.Router()
+var router = express.Router();
 const ArticlesModel = require('../models/articles.js');
 const UsersModel = require('../models/users');
 
-// displauying  editors page
-router.get("/", async function(req, res)
-{
-  if (!req.session.username || req.session.level !== 'editor') {
+// displaying editors page
+router.get("/", async function(req, res) {
+    
+    if (!req.session.username || req.session.level !== 'editor') {
         req.session.login_error = "You must be an editor to access this page.";
+        // Stop here and redirect
+        return res.redirect("/home"); 
+    }
 
+    try {
         const allUsers = await UsersModel.getAllUsers(); 
         const allArticles = await ArticlesModel.getAllArticles();
 
-            //passing list of users and articles to template to dosplay:
-            req.TPL.allUsers = allUsers;
-            req.TPL.allArticles = allArticles;
-            req.TPL.username = req.session.username;
+        //list of users and articles to template to display
+        req.TPL.allUsers = allUsers;
+        req.TPL.allArticles = allArticles;
+        req.TPL.username = req.session.username;
 
-            res.render("editors", req.TPL);
-
-        return res.redirect("/login");
+        // rendering the page and return
+        return res.render("editors", req.TPL);
+    } catch (err) {
+        console.error("Error loading editor data:", err);
+        return res.redirect("/home");
     }
-
-  res.render("editors", req.TPL);
 });
 
 module.exports = router;
